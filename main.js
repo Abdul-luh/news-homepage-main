e83a558daf0b872627b39f96a8c44f63ec3ac3b2
Abdullah <abdulluhodulate@gmail.com>
2023-01-29 04:09:37 +0100

    added a nav

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function myFunction() {
-	let x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+function openMenuFunc() {
+	document.getElementById("sideNavMenu").style.width = "70vw";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeMenuFunc() {
+	document.getElementById("sideNavMenu").style.width = "0";
+	document.body.style.backgroundColor = "white";
 }
